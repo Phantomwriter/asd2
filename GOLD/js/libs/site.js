e96d80e542dddcbdbf7bd88872a75b0ab6831059
Phantomwritergit config --global user.nametakeovertheworld.livingston7@gmail.comgit config --global user.name takeovertheworld.livingston7@gmail.com <takeovertheworld.livingston7@gmail.comgit config --global user.name takeovertheworld.livingston7@gmail.com>
2012-11-26 22:41:08 -0500

    week one refactored code

---
+++ GOLD/js/libs/site.js
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Jquery video file</title>
+</head>
+
+<body id="index" class="home">
+
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"</script>
+<!--jquery plugin goes here-->
+<script type="text/javascript" src="js/site.js"</script>
+
+
+</body>
+</html>
+
+$('#onlineGames').on('pageinit', function(){
+	console.log("onlineGames page loaded!");
+});
+
+$('#consoleGames').on('pageinit', function(){
+	console.log("Console Game Page loaded!");
+});
+
+$('#phoneApps').on('pageinit', function(){
+	console.log("Phone App  page loaded!");
+});
+
+$('#cardGames').on('pageinit', function(){
+	console.log("card Game  page loaded!");
+});
+
+$('#manualGames').on('pageinit', function(){
+	console.log("Manual Games page loaded!");
+});
+
+$('#about').on('pageinit', function(){
+	console.log("About Gameworld page loaded!");
+});
+
+$('#myPage').on('pageinit', function(){
+	console.log("My page loaded!");
+});
+
+$('#dealsPage').on('pageinit', function(){
+	console.log("Deals page loaded!");
+});
+
+$('#news').on('pageinit', function(){
+	console.log("News Stream page loaded!");
+});
+$('#signUpPageErrors').on('pageinit', function(){
+	console.log("Sign up form validator page loaded!");
+});
+$('#errorPage').on('pageinit', function(){
+	console.log("404 Page loaded!");
+});
